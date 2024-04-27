f51cc9c3ec6143fe69ed9c30003665883fe3299f
Krishna311203 <krishnashrivastava84@gmail.com>
2024-04-27 22:45:43 +0530

    passport configured

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
+import express from "express";
+import http from "http";
+import cors from "cors";
 import dotenv from "dotenv";
+import passport from "passport";
+import session from "express-session";
+import connectMongo from 'connect-mongodb-session'
 
-import mergedTypeDefs from "./typeDefs/index.js"
-import mergedResolvers from "./resolvers/index.js"
-import { connectDB } from './db/connectDB.js';
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { buildContext } from "graphql-passport";
 
-dotenv.config()
+import mergedTypeDefs from "./typeDefs/index.js";
+import mergedResolvers from "./resolvers/index.js";
+import { connectDB } from "./db/connectDB.js";
+import { configurePassport } from "./passport/passport.config.js";
+
+dotenv.config();
+configurePassport();
 
 const app = express();
 // Our httpServer handles incoming requests to our Express app.
@@ -18,11 +25,37 @@ const app = express();
 // enabling our servers to shut down gracefully.
 const httpServer = http.createServer(app);
 
+const MongoDBStore = connectMongo(session);
+
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
+});
+
+store.on("error", function (error) {
+  console.log(error);
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    },
+    store: store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 // Same ApolloServer initialization as before, plus the drain plugin
 // for our httpServer.
 const server = new ApolloServer({
-  typeDefs:mergedTypeDefs,
-  resolvers:mergedResolvers,
+  typeDefs: mergedTypeDefs,
+  resolvers: mergedResolvers,
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 // Ensure we wait for our server to start
@@ -31,14 +64,19 @@ await server.start();
 // Set up our Express middleware to handle CORS, body parsing,
 // and our expressMiddleware function.
 app.use(
-  '/',
-  cors(),
+  "/",
+  cors(
+    {
+      origin: "http://localhost:3000",
+      credentials: true,
+    }
+  ),
   express.json(),
   // expressMiddleware accepts the same arguments:
   // an Apollo Server instance and optional configuration options
   expressMiddleware(server, {
-    context: async ({ req }) => ({ token: req.headers.token }),
-  }),
+    context: async ({ req, res }) => buildContext({ req, res }),
+  })
 );
 
 // Modified server startup
