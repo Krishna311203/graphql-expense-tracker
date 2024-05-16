4f9eec9e5b8e63cc82a0941d4c2abcf2bd5fac44
Krishna311203 <krishnashrivastava84@gmail.com>
2024-05-16 23:28:24 +0530

    refetch error solved

--- frontend/src/pages/TransactionPage.jsx
+++ frontend/src/pages/TransactionPage.jsx
@@ -1,7 +1,7 @@
 import { useMutation, useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { GET_TRANSACTION} from "../graphql/queries/transaction.query";
+import { GET_TRANSACTION, GET_TRANSACTION_STATISTICS} from "../graphql/queries/transaction.query";
 import { UPDATE_TRANSACTION } from "../graphql/mutations/transaction.mutation";
 import toast from "react-hot-toast";
 import TransactionFormSkeleton from "../components/skeletons/TransactionFormSkeleton";
@@ -15,7 +15,7 @@ const TransactionPage = () => {
 
 	const [updateTransaction, { loading: loadingUpdate }] = useMutation(UPDATE_TRANSACTION, {
 		// https://github.com/apollographql/apollo-client/issues/5419 => refetchQueries is not working, and here is how we fixed it
-		refetchQueries: [ 'getTransactions','GetTransactionStatistics'],
+		refetchQueries: [{query:GET_TRANSACTION_STATISTICS} ],
 	});
 
 	const [formData, setFormData] = useState({
